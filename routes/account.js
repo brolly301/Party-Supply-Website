e9b0b199c0161493b7ee298bf79866b8d7b9fb27
Marc Brolly <marcrobertjohn@gmail.com>
2023-02-22 15:41:42 +0000

    Add account edit page and put routes, email working but can't update username yet

--- routes/account.js
+++ routes/account.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const router = express.Router();
 const catchAsync = require("../utilities/catchAsync");
+const User = require('../models/user')
 
-router.get("/", (req, res) => {
+router.get("/:username", (req, res) => {
     res.render("pages/account");
   });
+
+router.get("/:username/:edit", (req, res) => {
+    res.render("pages/accountEdit");
+  });
+
+router.put("/:username", catchAsync(async(req, res) => {
+  const {username} = req.params
+  await User.findOneAndUpdate(username, {...req.body})
+  res.redirect(username);
+}))
+
+
+
+
 
   module.exports = router;
