52542459de7b7dd9d6c3559971243306c7404629
Marc Brolly <marcrobertjohn@gmail.com>
2023-02-22 22:01:30 +0000

    Add order model and checkout details page

---
+++ models/order.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    customerName: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    phoneNumber: {
+        type: Number
+    },
+    address: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    postcode: {
+        type: String
+    },
+    products: {
+        type: []
+    },
+    price: {
+        type: Number
+    }
+})
+
+module.exports = mongoose.model('Order', OrderSchema)
--- routes/checkout.js
+++ routes/checkout.js
@@ -2,9 +2,17 @@ const express = require('express')
 const router = express.Router();
 const catchAsync = require("../utilities/catchAsync");
 const User = require('../models/user')
+const Order = require('../models/order')
 
 router.get("/", (req, res) => {
     res.render("pages/checkout");
   });
+
+router.post("/", async(req, res) => {
+     const deliveryDetails = req.body
+     const newOrder = new Order(deliveryDetails)
+     newOrder.save()
+     res.render("pages/checkout");
+  });  
 
   module.exports = router;
