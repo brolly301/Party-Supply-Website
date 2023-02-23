2e10c2878ab35dcb10d72b23d12804565771a84a
Marc Brolly <marcrobertjohn@gmail.com>
2023-02-23 20:31:33 +0000

    Update

--- routes/checkout.js
+++ routes/checkout.js
@@ -5,17 +5,36 @@ const User = require('../models/user')
 const Order = require('../models/order')
 const Basket = require('../models/basket')
 
-router.get("/", (req, res) => {
+let array = []
+
+router.get("/", async(req, res) => {
+  const {username} = req.user
+  
+
+  const basketItems = await Basket.find({username: username})
+  for(let i = 0; i<basketItems.length; i++) {
+    array.push(basketItems[i].product)
+  }
+
+  console.log(array)
+
     res.render("pages/checkout");
   });
 
 router.post("/", async(req, res) => {
      const deliveryDetails = req.body
      const {username} = req.user
-     console.log(username)
+  
+     const basketItems = await Basket.find({username: username})
      const newOrder = new Order(deliveryDetails)
-     newOrder.save()
-     await newOrder.updateOne({username:username})
+     await newOrder.save()
+ 
+     console.log(newOrder)
+     console.log(basketItems)
+    
+     await newOrder.updateOne({products: [array]})
+     console.log(basketItems[0].product[0])
+   
      res.render("pages/checkout");
   });  
 
