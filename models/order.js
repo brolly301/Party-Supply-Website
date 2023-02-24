cd5f627edf351616a064d4271669eb165baff5f3
Marc Brolly <marcrobertjohn@gmail.com>
2023-02-24 17:31:21 +0000

    Order attempt

--- models/order.js
+++ models/order.js
@@ -26,9 +26,10 @@ const OrderSchema = new Schema({
     postcode: {
         type: String
     },
-    products: {
-        type: []
-    },
+    basket: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }],
     price: {
         type: Number
     }
--- routes/checkout.js
+++ routes/checkout.js
@@ -3,15 +3,25 @@ const router = express.Router();
 const catchAsync = require("../utilities/catchAsync");
 const User = require('../models/user')
 const Order = require('../models/order')
+const Basket = require('../models/basket')
 
-router.get("/", (req, res) => {
+router.get("/", async(req, res) => {
+
+
     res.render("pages/checkout");
   });
 
 router.post("/", async(req, res) => {
+     
+
+  const {username} = req.user
+  let basketItem = await Basket.findOne({username: username }).populate('products')
+  let array = basketItem.products
+     console.log(array)
      const deliveryDetails = req.body
      const newOrder = new Order(deliveryDetails)
      newOrder.save()
+     newOrder.update(basketItem.products)
      res.render("pages/checkout");
   });  
 
