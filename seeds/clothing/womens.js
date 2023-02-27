78f49c70f4bad0dc2a1dc05a4076bbde55ae470c
Marc Brolly <marcrobertjohn@gmail.com>
2023-02-27 16:28:32 +0000

    Update changes

--- seeds/clothing/womens.js
+++ seeds/clothing/womens.js
@@ -4,7 +4,7 @@ module.exports = [{
     price: `15`,
     image: `https://images.unsplash.com/photo-1557291483-8677a3281655?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`,
     color: 'Red',
-    category: 'Womens'
+    category: 'Clothing'
 },
 {
     name: `Green Balloons`,
@@ -12,56 +12,97 @@ module.exports = [{
     price: `15`,
     image: `https://images.unsplash.com/photo-1572355286138-8dae8e7ba20d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80`,
     color: 'Green',
-    category: 'Womens'
-}, {
-    name: `Blue Balloons`,
-    description: `Collection of 12 Blue Balloons`,
-    price: `15`,
-    image: `https://images.unsplash.com/photo-1554719956-5c151ab80680?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ymx1ZSUyMGJhbGxvb25zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60`,
-    color: 'Blue',
-    category: 'Womens'
-}, {
-    name: `Black Balloons`,
-    description: `Collection of 12 Black Balloons`,
-    price: `15`,
-    image: `https://img.freepik.com/free-photo/top-view-black-balloons_23-2148502434.jpg?w=740&t=st=1676452441~exp=1676453041~hmac=cef6f16142637849ac1876bef8d71436f8d20598b062218d275eeaffc6dce2d7`,
-    color: 'Black',
-    category: 'Womens'
-}, {
-    name: `Purple Balloons`,
-    description: `Collection of 12 Purple Balloons`,
-    price: `15`,
-    image: `https://img.freepik.com/free-photo/balloons-color-year-2022_23-2149217415.jpg?w=360&t=st=1676452523~exp=1676453123~hmac=cd3b68a5ee388d94bfc34bbf4044651bcb0f0e91fa5dd8aa203eb44c17c16ca5`,
-    color: 'Purple',
-    category: 'Womens'
-}, {
-    name: `Pink Balloons`,
-    description: `Collection of 12 Pink Balloons`,
-    price: `15`,
-    image: `https://img.freepik.com/free-photo/pink-birthday-air-balloons-pink-background-with-mockup_8353-5748.jpg?w=826&t=st=1676452555~exp=1676453155~hmac=791d89e3e4683148a07278e0926d415b56a410645fcb99c690680c98fd67e270`,
-    color: 'Pink',
-    category: 'Womens'
-},
-{
-    name: `White Balloons`,
-    description: `Collection of 12 White Balloons`,
-    price: `15`,
-    image: `https://img.freepik.com/free-photo/balloons_23-2148164785.jpg?w=740&t=st=1676452577~exp=1676453177~hmac=f9ee98b84b8b015ee649d82b6571342387f41b99fb0335997f24e28bc45386b0`,
-    color: 'White',
-    category: 'Womens'
-}, {
-    name: `Silver Balloons`,
-    description: `Collection of 12 Grey Balloons`,
-    price: `15`,
-    image: `https://img.freepik.com/premium-photo/silver-photo-zone-with-air-balloons-new-year-background_175086-1003.jpg?w=900`,
-    color: 'Grey',
-    category: 'Womens'
-},
-{
-    name: `Gold Balloons`,
-    description: `Collection of 12 Yellow Balloons`,
-    price: `15`,
-    image: `https://img.freepik.com/free-photo/christmas-concept-with-balls-copy-space_23-2148731069.jpg?w=740&t=st=1676452638~exp=1676453238~hmac=2439baec404da254db8b892efba52edd55a7737110b2d0c9d807b1716d2d1c35`,
-    color: 'Yellow',
-    category: 'Womens'
-},]
+    category: 'Clothing'
+}]
+
+module.exports = [{
+    name: `Mens`,
+    description: `Mens Products`,
+    image: `https://img.freepik.com/free-photo/guy-with-painted-face-classic-suit-poses-with-false-mustache-leaning-guitar_197531-17157.jpg?w=900&t=st=1676558350~exp=1676558950~hmac=4b47621b91c4735fdbb0d5910efcb4d82c207f8697b98e5ad3624063276be612`,
+    category: 'Clothing',
+    clothing: [{
+        name: 'Mens Top',
+        color: 'black'
+    },{
+        name: 'Mens Top',
+        color: 'black'
+    },{
+        name: 'Mens Top',
+        color: 'black'
+    },{
+        name: 'Mens Top',
+        color: 'black'
+    },{
+        name: 'Mens Top',
+        color: 'black'
+    },{
+        name: 'Mens Top',
+        color: 'black'
+    },{
+        name: 'Mens Top',
+        color: 'black'
+    },{
+        name: 'Mens Top',
+        color: 'black'
+    },]
+},{
+    name: `Womens`,
+    description: `Womens Products`,
+    image: `https://img.freepik.com/free-photo/interested-long-haired-girl-holding-orange-pumpkin-halloween-photoshoot-indoor-photo-appealing-blonde-lady-witch-costume_197531-15961.jpg?w=900&t=st=1676558375~exp=1676558975~hmac=9256800af381b5a2114f4a2851eb6765add4e87eddd4f33fd3f6316daa397fcd`,
+    category: 'Clothing',
+    clothing: [{
+        name: 'Womens Top',
+        color: 'pink'
+    },{
+        name: 'Womens Top',
+        color: 'pink'
+    },{
+        name: 'Womens Top',
+        color: 'pink'
+    },{
+        name: 'Womens Top',
+        color: 'pink'
+    },{
+        name: 'Womens Top',
+        color: 'pink'
+    },{
+        name: 'Womens Top',
+        color: 'pink'
+    },{
+        name: 'Womens Top',
+        color: 'pink'
+    },{
+        name: 'Womens Top',
+        color: 'pink'
+    },]
+},{
+    name: `Kids`,
+    description: `Kids Products`,
+    image: `https://img.freepik.com/free-photo/gray-backgrounds-hero-studio-shot-child_1134-1430.jpg?w=826&t=st=1676558447~exp=1676559047~hmac=86b8a156fb911d72b76d52e422863239fe8ac1a1ba419fea35afbcff2abf880f`,
+    category: 'Clothing',
+    clothing: [{
+        name: 'Kids Top',
+        color: 'blue'
+    },{
+        name: 'Kids Top',
+        color: 'blue'
+    },{
+        name: 'Kids Top',
+        color: 'blue'
+    },{
+        name: 'Kids Top',
+        color: 'blue'
+    },{
+        name: 'Kids Top',
+        color: 'blue'
+    },{
+        name: 'Kids Top',
+        color: 'blue'
+    },{
+        name: 'Kids Top',
+        color: 'blue'
+    },{
+        name: 'Kids Top',
+        color: 'blue'
+    },]
+}]
