387264db2432e7bf888976c08229f282eb7aa19f
Marc Brolly <marcrobertjohn@gmail.com>
2023-02-27 20:03:04 +0000

    Fully Working version after merge

--- routes/checkout.js
+++ routes/checkout.js
@@ -5,37 +5,18 @@ const User = require('../models/user')
 const Order = require('../models/order')
 const Basket = require('../models/basket')
 
-let array = []
-
 router.get("/", async(req, res) => {
-  const {username} = req.user
-  
-
-  const basketItems = await Basket.find({username: username})
-  for(let i = 0; i<basketItems.length; i++) {
-    array.push(basketItems[i].product)
-  }
-
-  console.log(array)
-
-    res.render("pages/checkout");
+  res.render("pages/checkout/checkout");
   });
 
 router.post("/", async(req, res) => {
      const deliveryDetails = req.body
      const {username} = req.user
-  
-     const basketItems = await Basket.find({username: username})
-     const newOrder = new Order(deliveryDetails)
+     const basketItems = await Basket.find({username})
+     const newOrder = new Order({...deliveryDetails,username, basket:basketItems[0].products })
      await newOrder.save()
- 
-     console.log(newOrder)
-     console.log(basketItems)
-    
-     await newOrder.updateOne({products: [array]})
-     console.log(basketItems[0].product[0])
-   
-     res.render("pages/checkout");
+
+     res.render("pages/checkout/checkoutComplete");
   });  
 
   module.exports = router;
