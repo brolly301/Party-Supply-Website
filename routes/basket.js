90f02de4a018852782ac8e38905b35a543c7421b
Marc Brolly <marcrobertjohn@gmail.com>
2023-02-28 17:03:29 +0000

    Update session store

--- routes/basket.js
+++ routes/basket.js
@@ -6,29 +6,49 @@ const Product = require("../models/product");
 const Basket = require("../models/basket");
 
 router.get("/",  catchAsync(async (req, res) => {
-    const {username} = req.user
-    const basketItems = await Basket.find({username: username}).populate("products")
-    let total = 0;
-    res.render("pages/checkout/basket", {basketItems, total});
+
+ let basket = []
+  for (let i =0; i< req.session.basket.products.length; i++) {
+     const basketItems = await Product.findById(req.session.basket.products[i])
+     basket.push(basketItems)
+  }
+  
+
+  res.render("pages/checkout/basket", {basket});
   }));
+ 
 
 router.post("/", catchAsync(async (req, res) => {
-    const username = req.user.username
-    const id = req.body.id
-    const product = await Product.findById(id)
-     
-    let basketItem = await Basket.findOne({ username }).populate('products');
-    if (basketItem) {
-      basketItem.products.push(product)
-      basketItem.save()
-      req.flash('success', 'Added to Basket')
-      res.redirect('back')
-    } else {
-      const newBasket = new Basket({username, products: product});
-      newBasket.save()
-      req.flash('success', 'Added to Basket')
-      res.redirect('back')
+   
+   const {id} = req.body
+   const basket = new Basket(req.session.basket)
+    Product.findById(id, function (error, product) {
+    if (error) {
+      return res.redirect('back')
     }
+    basket.products.push(product._id)
+    req.session.basket = basket
+    console.log(req.session)
+    res.redirect('/')
+   })
+
+ 
+
+    // const username = req.user.username
+    // const id = req.body.id
+    // const product = await Product.findById(id)
+    // let basketItem = await Basket.findOne({ username }).populate('products');
+    // if (basketItem) {
+    //   basketItem.products.push(product)
+    //   basketItem.save()
+    //   req.flash('success', 'Added to Basket')
+    //   res.redirect('back')
+    // } else {
+    //   const newBasket = new Basket({username, products: product});
+    //   newBasket.save()
+    //   req.flash('success', 'Added to Basket')
+    //   res.redirect('back')
+    // }
   }))
 
  router.delete("/", catchAsync(async (req, res) => {
