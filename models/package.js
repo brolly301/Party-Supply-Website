50cc4acdb10592118b74b31f95748af6cc97c9cc
Marc Brolly <marcrobertjohn@gmail.com>
2023-03-05 01:35:02 +0000

    Fix positioning of pages moving due to scroll bar

--- models/package.js
+++ models/package.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const PackageSchema = new Schema({
     name: String,
     description: String,
+    image: String,
     products: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Product'
