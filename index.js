3a9578b69775d3720d5e3dcf9c7b6acbed4a62f9
Marc Brolly <marcrobertjohn@gmail.com>
2023-03-13 12:10:28 +0000

    Fix basket quantity not updating with there is no basket and add validation for register form

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use((req,res,next) => {
   res.locals.error = req.flash('error')
   res.locals.signedInUser = req.user
   res.locals.session = req.session
-  res.locals.params = req.params
+
   // res.locals.totalBasketPrice = req.session.basket.totalBasketPrice
   next()
 })
