c0571cc9eab4d8caa2453d9d1e6f65dc3dccb67e
Marc Brolly <marcrobertjohn@gmail.com>
2023-03-14 23:33:38 +0000

    Add order confirmation email with nodemailer, update pagination on product page

--- routes/checkout.js
+++ routes/checkout.js
@@ -7,6 +7,7 @@ const Basket = require('../models/basket')
 const Product = require('../models/product')
 const Stripe = require('stripe')
 const stripe = Stripe("sk_test_51MgRG0AYx3n7HkYcaolBrw29SN2beilaDLgGCEYSzRnnQOPYt2BCM86W5j3DGvCSGcgeCwc4VbB8I5IavwYddYON008lZ0AzGN");
+const nodemailer = require('nodemailer');
 require('dotenv').config()
 
 
@@ -29,11 +30,65 @@ router.post("/", async(req, res) => {
   if (!req.user) {
     const newOrder = new Order({...deliveryDetails, basket:[...req.session.basket.products]})
     await newOrder.save()
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "marcrobertjohn@gmail.com",
+        pass: "qklbtfcwnloxeckw"
+      }
+    })
+  
+    const options = {
+      from: "marcrobertjohn@gmail.com",
+      to: req.body.email,
+      subject: "Order Confirmation",
+      text: `This is a test order confirmation!`
+    }
+  
+    transporter.sendMail(options, function (err, info) {
+      if (err) {
+        req.flash('error', 'Message has not been sent')
+        console.log(err)
+      }
+       req.flash('success', 'Message has been sent')
+       console.log("Sent:" + info.messageId)
+       return res.redirect('back')
+    
+    })
+
     return res.render("pages/checkout/payment");
   }
   const {username} = req.user
   const newOrder = new Order({...deliveryDetails, username, basket:[...req.session.basket.products]})
   await newOrder.save()
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "marcrobertjohn@gmail.com",
+      pass: "qklbtfcwnloxeckw"
+    }
+  })
+
+  const options = {
+    from: "marcrobertjohn@gmail.com",
+    to: req.body.email,
+    subject: "Order Confirmation",
+    text: `This is a test order confirmation!`
+  }
+
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      req.flash('error', 'Message has not been sent')
+      console.log(err)
+    }
+     req.flash('success', 'Message has been sent')
+     console.log("Sent:" + info.messageId)
+     return res.redirect('back')
+  
+  })
+
   res.render("pages/checkout/payment");
   });  
 
