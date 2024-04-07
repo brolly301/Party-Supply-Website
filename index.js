b8bf2ad4c1cc579be8867613e5624ea69a73d21c
Marc Brolly <marcrobertjohn@gmail.com>
2024-04-07 16:20:27 +0100

    Add responsive design to account, checkout, wishlist, basket, search, home and marketplace pages

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
-if (process.env.NODE_ENV !== "production")  {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-console.log(process.env.CLOUDINARY_SECRET)
+console.log(process.env.CLOUDINARY_SECRET);
 
 const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utilities/ExpressError")
+const ExpressError = require("./utilities/ExpressError");
 const methodOverride = require("method-override");
-const flash = require ('connect-flash')
-const session = require ('express-session')
-const passport = require('passport')
-const LocalAuth = require('passport-local')
-const User = require('./models/user')
-const MongoStore = require('connect-mongo')
-const mongoSanitize = require('express-mongo-sanitize')
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+const LocalAuth = require("passport-local");
+const User = require("./models/user");
+const MongoStore = require("connect-mongo");
+const mongoSanitize = require("express-mongo-sanitize");
 
-const packages = require ('./routes/packages')
-const marketplace = require ('./routes/marketplace')
-const basket = require ('./routes/basket')
-const account = require ('./routes/account')
-const authorisation = require ('./routes/authorisation')
-const checkout = require ('./routes/checkout')
-const products = require ('./routes/products')
+const packages = require("./routes/packages");
+const marketplace = require("./routes/marketplace");
+const basket = require("./routes/basket");
+const account = require("./routes/account");
+const authorisation = require("./routes/authorisation");
+const checkout = require("./routes/checkout");
+const products = require("./routes/products");
 
 //Mongoose Setup
 mongoose.set("strictQuery", false);
 mongoose
-  .connect("mongodb://127.0.0.1:27017/partySupplies", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://marcrobertjohn:PartySupplies@cluster0.buzfoot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("MONGO CONNECTION OPEN!!!");
   })
@@ -41,72 +44,79 @@ mongoose
     console.log(err);
   });
 
-const sessionOptions = {secret: 'Test', resave: false, saveUninitialized: true, store: MongoStore.create({mongoUrl: "mongodb://127.0.0.1:27017/partySupplies"}),
-cookie: {
-  httpOnly: true,
-  expires: Date.now() + 1000 * 60 * 60 * 24 * 2,
-  maxAge: 120 * 60 * 1000
-}}
+const sessionOptions = {
+  secret: "Test",
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl:
+      "mongodb+srv://marcrobertjohn:PartySupplies@cluster0.buzfoot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  }),
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 2,
+    maxAge: 120 * 60 * 1000,
+  },
+};
 
-app.use(session(sessionOptions))
-app.use(flash())
+app.use(session(sessionOptions));
+app.use(flash());
 
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalAuth(User.authenticate()))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalAuth(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next) => {
-  res.locals.success = req.flash('success')
-  res.locals.error = req.flash('error')
-  res.locals.signedInUser = req.user
-  res.locals.session = req.session
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.signedInUser = req.user;
+  res.locals.session = req.session;
 
   // res.locals.totalBasketPrice = req.session.basket.totalBasketPrice
-  next()
-})
+  next();
+});
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
-
-app.use(express.json())
+app.use(express.json());
 
 //Routes
-app.use('/packages', packages)
-app.use('/marketplace', marketplace)
-app.use('/basket', basket)
-app.use('/account', account)
-app.use('/', authorisation)
-app.use('/checkout', checkout)
-app.use('/', products)
+app.use("/packages", packages);
+app.use("/marketplace", marketplace);
+app.use("/basket", basket);
+app.use("/account", account);
+app.use("/", authorisation);
+app.use("/checkout", checkout);
+app.use("/", products);
 
 //Additional routes & middleware
 app.get("/", (req, res) => {
   res.render("pages/home");
 });
 
-app.get("/hey/config", (req,res) => {
+app.get("/hey/config", (req, res) => {
   res.send({
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
-  })
-})
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
 
-app.all('*', (req,res,next) => {
-   next(new ExpressError('Page Not Found', 404)) 
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
 app.use((err, req, res, next) => {
-  const {statusCode = 500} = err
-  res.status(statusCode).render('pages/error', {err})
-})
+  const { statusCode = 500 } = err;
+  res.status(statusCode).render("pages/error", { err });
+});
 
 //Setting up the applications listener
 app.listen(3000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -6,7 +6,7 @@ const decorations = require("./decorations");
 const themes = require("./themes");
 const fancyDress = require("./fancyDress");
 const packages = require("./packages");
-const {kids, mens, womens} = require("./clothing");
+const { kids, mens, womens } = require("./clothing");
 const easter = require("./themes/easter");
 const halloween = require("./themes/halloween");
 const christmas = require("./themes/christmas");
@@ -17,40 +17,41 @@ const birthdays = require("./themes/birthdays");
 
 mongoose.set("strictQuery", false);
 mongoose
-    .connect("mongodb://127.0.0.1:27017/partySupplies", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!");
-    })
-    .catch((err) => {
-        console.log("OH NO MONGO ERROR!");
-        console.log(err);
-    });
+  .connect(
+    "mongodb+srv://marcrobertjohn:PartySupplies@cluster0.buzfoot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH NO MONGO ERROR!");
+    console.log(err);
+  });
 
 const seedDB = async () => {
-
-
-    await Basket.deleteMany({});
-    await Product.deleteMany({});
-    await Product.insertMany(balloons);
-    await Product.insertMany(decorations);
-    await Product.insertMany(themes);
-    await Product.insertMany(fancyDress);
-    await Product.insertMany(packages);
-    await Product.insertMany(kids);
-    await Product.insertMany(mens);
-    await Product.insertMany(womens);
-    await Product.insertMany(easter);
-    await Product.insertMany(halloween);
-    await Product.insertMany(christmas);
-    await Product.insertMany(paddysDay);
-    await Product.insertMany(valentines);
-    await Product.insertMany(marketplace);
-    await Product.insertMany(birthdays);
-}
+  // await Basket.deleteMany({});
+  // await Product.deleteMany({});
+  await Product.insertMany(balloons);
+  await Product.insertMany(decorations);
+  await Product.insertMany(themes);
+  await Product.insertMany(fancyDress);
+  await Product.insertMany(packages);
+  await Product.insertMany(kids);
+  await Product.insertMany(mens);
+  await Product.insertMany(womens);
+  await Product.insertMany(easter);
+  await Product.insertMany(halloween);
+  await Product.insertMany(christmas);
+  await Product.insertMany(paddysDay);
+  await Product.insertMany(valentines);
+  await Product.insertMany(marketplace);
+  await Product.insertMany(birthdays);
+};
 
 seedDB().then(() => {
-    mongoose.connection.close()
+  mongoose.connection.close();
 });
